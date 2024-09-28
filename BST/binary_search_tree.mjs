fe6580d3f48917aaa26c1a7645deab89175fb0d1
AnnaZhizhiashvili <ana.zhizhiashvili@protonmail.com>
2024-09-28 13:44:55 +0400

    add BST implementation

---
+++ BST/binary_search_tree.mjs
@@ -0,0 +1,57 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null;
+    }
+    insert(value) {
+        const newNode = new Node(value);
+        if(this.root === null) {
+            this.root = newNode;
+            return this;
+        }
+
+        let temp = this.root;
+        while(true) {
+            if(temp.value === newNode.value) {
+                return undefined;
+            }
+            if(newNode.value > temp.value) {
+                if(temp.right === null) {
+                    temp.right = newNode;
+                    return this;
+                }
+                temp = temp.right;
+            } else {
+                if (temp.left === null) {
+                    temp.left = newNode;
+                    return this;
+                }
+                temp = temp.left;
+            }
+        }
+    }
+
+    contains(value){
+        if(this.root === null) return false;
+
+        let temp = this.root;
+
+        while(temp) {
+            if (value < temp.value) {
+                temp = temp.left;
+            } else if (value > temp.value) {
+                temp = temp.right
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
